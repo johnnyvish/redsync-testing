c0b33770ac4c248dba5ca9bfe0222a4611eefb62
Johnny  Vishnevskiy <johnnyvishnevskiy@Johnnys-Air.fios-router.home>
2024-01-25 01:08:05 -0500

    update

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-t from-rose-600 via-red-500 to-red-600 text-white">
       <div className="flex flex-col items-center justify-center space-y-12 mt-[240px]">
-        <button className={`h-[160px] w-[160px] rounded-full shadow-2xl bg-white ${recording ? '' : 'agent-circle'}`}
+        <button className={`h-[160px] w-[160px] rounded-full shadow-2xl bg-white ${recording ? 'agent-circle' : ''}`}
           onClick={toggleRecording}>
         </button>
         {recording && (
