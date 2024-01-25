fc97bb5089c76bf4f0b17a9f865bee1d6851354b
Johnny  Vishnevskiy <johnnyvishnevskiy@Johnnys-Air.fios-router.home>
2024-01-25 12:56:42 -0500

    update

--- app/page.js
+++ app/page.js
@@ -6,8 +6,14 @@ export default function Home() {
   const [recording, setRecording] = useState(false);
   const [showLoadingDots, setShowLoadingDots] = useState(false);
   const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showImages, setShowImages] = useState(false);
   const prevLoadingState = useRef(showLoadingDots);
   const firstClickRef = useRef(true);
+  const imageDisplayTimeoutRef = useRef(null);
+  const [isPlayingPartTwo, setIsPlayingPartTwo] = useState(false);
+
+  
   const audioList = [
     '/1.mp3',
     '/2.mp3',
@@ -15,7 +21,11 @@ export default function Home() {
     '/4.mp3',
     '/5.mp3',
     '/6.mp3',
-    // Add more audio files here if needed
+  ];
+  const imageList = [
+    '/image1.jpg',
+    '/image2.jpg',
+    '/image3.jpg',
   ];
 
   function toggleRecording() {
@@ -25,42 +35,80 @@ export default function Home() {
       setCurrentAudioIndex(1);
     } else {
       setRecording(!recording);
+      setShowImages(false); // Hide images when recording starts
       if (recording) {
         setShowLoadingDots(true);
-        const randomTime = Math.random() * (6000 - 4000) + 4000;
+        const randomTime = Math.random() * (4000 - 3000) + 3000;
         setTimeout(() => setShowLoadingDots(false), randomTime);
       }
     }
-  }
+  }  
 
   useEffect(() => {
     const handleKeyPress = (event) => {
       if (event.key === ' ') {
-        event.preventDefault(); // Prevent the default spacebar action
+        event.preventDefault();
         toggleRecording();
       }
     };
 
     window.addEventListener('keydown', handleKeyPress);
-
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
-  }, [recording]); // Add recording as a dependency
+  }, [recording]);
 
   useEffect(() => {
-    if (prevLoadingState.current && !showLoadingDots && !firstClickRef.current) {
+    if (prevLoadingState.current && !showLoadingDots && !firstClickRef.current && !isPlayingPartTwo) {
       const audio = new Audio(audioList[currentAudioIndex]);
       audio.play();
-      const nextIndex = (currentAudioIndex + 1) % audioList.length;
-      setCurrentAudioIndex(nextIndex);
+
+      if (currentAudioIndex === 2) {
+        audio.onended = () => {
+          setShowLoadingDots(true);
+          setIsPlayingPartTwo(true);
+
+          setTimeout(() => {
+            const nextAudio = new Audio('/3_part_2.mp3');
+            nextAudio.play();
+            nextAudio.onended = () => {
+              setIsPlayingPartTwo(false);
+            };
+
+            setShowLoadingDots(false);
+            setShowImages(true);
+            setCurrentImageIndex(0);
+          }, 5000);
+        };
+      } else {
+        setShowImages(false);
+      }
+
+      if (!isPlayingPartTwo) {
+        const nextIndex = (currentAudioIndex + 1) % audioList.length;
+        setCurrentAudioIndex(nextIndex);
+      }
     }
     prevLoadingState.current = showLoadingDots;
-  }, [showLoadingDots, currentAudioIndex, audioList]);
+  }, [showLoadingDots, currentAudioIndex, isPlayingPartTwo]);
 
   useEffect(() => {
-    return () => clearTimeout(showLoadingDots);
-  }, [recording]);
+    let imageRotationInterval = null;
+  
+    if (showImages) {
+      imageRotationInterval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+      }, 7000); // Rotate images every 5 seconds
+    }
+  
+    return () => {
+      if (imageRotationInterval) {
+        clearInterval(imageRotationInterval);
+      }
+    };
+  }, [showImages, imageList.length]);
+  
+
 
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-t from-rose-600 via-red-500 to-red-600 text-white">
@@ -80,6 +128,9 @@ export default function Home() {
             <div></div>
             <div></div>
           </div>
+        )}
+        {showImages && (
+          <img className="w-[200px] rounded-2xl floating"src={imageList[currentImageIndex]} alt="Displayed Image" />
         )}
       </div>
     </main>
